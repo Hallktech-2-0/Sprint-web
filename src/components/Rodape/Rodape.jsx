90914bf4b6b756f6f2240a1a66693782e0e121dd
RosaSarah <sarahrosa834@gmail.com>
2023-11-13 01:47:07 -0300

    rodape

---
+++ src/components/Rodape/Rodape.jsx
@@ -0,0 +1,13 @@
+// components/Rodape.js
+const Rodape = () => {
+    return (
+      <footer className="bg-gray-900 text-gray-300 py-4">
+        <div className="container mx-auto text-center">
+          &copy; 2023 Haalk-Tech . Todos os direitos reservados.
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Rodape;
+  
